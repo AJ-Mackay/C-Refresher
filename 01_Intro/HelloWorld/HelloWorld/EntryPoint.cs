44c5d366b251167d3986f8ea9e642831ed6558c9
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 13:54:19 +0000

    Say Hello to the World! (Your First Program)

---
+++ 01_Intro/HelloWorld/HelloWorld/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello world");
+        Console.WriteLine();
+        Console.WriteLine("I am learning C#");
+    }
+}
+
