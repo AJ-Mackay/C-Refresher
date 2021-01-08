9920d2308fe819b68560cedf8bd5e77c6ca14cc0
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 18:17:53 +0000

    forgot to start a new project, file returned to original end point

--- 06_Loops/WhileDoWhile/WhileDoWhile/EntryPoint.cs
+++ 06_Loops/WhileDoWhile/WhileDoWhile/EntryPoint.cs
@@ -6,7 +6,7 @@ class EntryPoint
     {
         Console.WriteLine("0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15...");
         Console.WriteLine();
-
+        
         Console.WriteLine("Please enter your first number:");
         int firstNumber = int.Parse(Console.ReadLine());
         Console.WriteLine("Please enter your second number:");
