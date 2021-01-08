a464ae2d1e60e4e0d151b6ad7ed7d15ca93afbfe
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 18:24:18 +0000

    Creating For Loops and The Difference With While Loops

---
+++ 06_Loops/ForLoops/ForLoops/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        for (int i = 10; i >= 1; i--)
+        {
+            if (i == 1)
+            {
+                Console.WriteLine("The for loop was executed {0} time!", i);
+            }
+            else
+            {
+                Console.WriteLine("The for loop was executed {0} times!", i);
+            }
+        }
+    }
+}
