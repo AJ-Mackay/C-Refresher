c39661ea9396ad8d44fc8edef2bf17384bcea1bc
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 18:13:01 +0000

    What is a Loop and Create a Simple While Loop

---
+++ 06_Loops/WhileDoWhile/WhileDoWhile/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        Console.WriteLine("0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15...");
+        Console.WriteLine();
+
+        Console.WriteLine("Please enter your first number:");
+        int firstNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter your second number:");
+        int endNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        while (firstNumber <= endNumber)
+        {
+            Console.WriteLine(firstNumber);
+            firstNumber++;
+        }
+    }
+}
