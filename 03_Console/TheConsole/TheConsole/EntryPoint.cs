6e54838539eb216723ae7539f6e784d895c0b92a
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 16:09:32 +0000

    Difference Between .Write and .WriteLine

---
+++ 03_Console/TheConsole/TheConsole/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        Console.Write("I am learning ");
+        Console.Write("C# from ");
+        Console.Write("Udemy.\n"); // "\n" starts a new line
+
+        Console.WriteLine(); // starts a new line
+        Console.WriteLine("I am learning ");
+        Console.WriteLine("C# from ");
+        Console.WriteLine("Udemy.\n");
+    }
+}
+
