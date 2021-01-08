7f43e3bd140327ac637f0a1eff304a2ed17331f9
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 15:13:44 +0000

    Store Anything Into A String!

---
+++ 02_Variables/Strings/Strings/Strings/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        string firstString = "C# Course";
+        string secondString = "for beginners";
+        string freeSpace = " ";
+
+        Console.WriteLine(firstString);
+        Console.WriteLine(secondString);
+
+        Console.WriteLine("I am taking my first " + firstString + freeSpace + secondString + " and I am really happy with what I am learning."); // concatenation
+        Console.WriteLine("I am taking my first {0}{1}{2} and I am really happy with what I am learning.", firstString, freeSpace, secondString);
+    }
+}
