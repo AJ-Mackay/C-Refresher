294469a53c90b47332e6168c058375092485ba9e
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 17:07:31 +0000

    The Logical Operators - To Be Or Not To Be

---
+++ 05_Conditionals/LogicalOperators/LogicalOperators/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        // Logical Operators:
+        // AND - &&
+        // OR - ||
+        // XOR - ^
+        // NOT - !
+
+        bool theTrue = true;
+        bool theFalse = false;
+
+        Console.WriteLine(theTrue && theFalse);
+        Console.WriteLine(theTrue || theFalse);
+        Console.WriteLine(theTrue ^ theFalse);
+        Console.WriteLine(!theTrue);
+        Console.WriteLine();
+        Console.WriteLine((theTrue || theFalse) && theFalse);
+        Console.WriteLine((theTrue || theFalse) && (theFalse ^ theTrue));
+    }
+}
