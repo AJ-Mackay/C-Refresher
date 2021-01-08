2483ffd1c820f34761a7f22e22973ba4d230bbb4
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 14:07:27 +0000

    Quick Tip - Input Values from the Console

---
+++ 01_Intro/InputValues/InputValues/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        // Console.WriteLine("What is your name?");
+        // string firstName = Console.ReadLine();
+        //
+        // Console.WriteLine(firstName);
+
+        // Console.WriteLine("How old are you?");
+        // int age = int.Parse(Console.ReadLine());
+        //
+        // Console.WriteLine(age);
+
+        int age = 0;
+
+        while (true)
+        {
+            Console.WriteLine("How old are you?");
+            age = int.Parse(Console.ReadLine());
+
+            if (age <= 10 && age >= 1)
+            {
+                break;
+            }
+
+            Console.WriteLine("Please input an age between 1 and 10");
+            Thread.Sleep(2000);
+        }
+
+        Console.WriteLine(age);
+    }
+}
