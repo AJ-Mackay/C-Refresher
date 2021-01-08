7a73a691a82b15465b539ba006fd1972e9878805
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 14:28:58 +0000

    Brief Overview of The Different Data Types

--- 02_Variables/DataTypes/DataTypes/DataTypes/EntryPoint.cs
+++ 02_Variables/DataTypes/DataTypes/DataTypes/EntryPoint.cs
@@ -6,9 +6,25 @@ class EntryPoint
         int number = 5;
 
         Console.WriteLine(number);
-        Console.WriteLine(number);
-        Console.WriteLine(number);
-        Console.WriteLine(number);
-        Console.WriteLine(number);
+        // Console.WriteLine(number);
+        // Console.WriteLine(number);
+        // Console.WriteLine(number);
+        // Console.WriteLine(number);
+
+        string sentence = "Hello World 3 ! @ # % ";
+
+        Console.WriteLine(sentence);
+
+        char letter = 'a';
+
+        Console.WriteLine(letter);
+
+        bool areEqual = true;
+
+        Console.WriteLine(areEqual);
+
+        float mathPI = 3.14F;
+
+        Console.WriteLine(mathPI);
     }
 }
