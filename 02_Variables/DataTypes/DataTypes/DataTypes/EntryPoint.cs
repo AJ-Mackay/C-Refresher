54348e376887047d1a498986331cb7a787db88f1
Paul Mackay <paul.m.mackay85@gmail.com>
2021-01-08 14:18:52 +0000

    What is a Variable and What is a Data Type?

---
+++ 02_Variables/DataTypes/DataTypes/DataTypes/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class EntryPoint
+{
+    static void Main()
+    {
+        int number = 5;
+
+        Console.WriteLine(number);
+        Console.WriteLine(number);
+        Console.WriteLine(number);
+        Console.WriteLine(number);
+        Console.WriteLine(number);
+    }
+}
